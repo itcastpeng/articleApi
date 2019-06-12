38554d784ba25fa4bb633c796186b4245c55d939
zhaoxinpeng <15531506906@sina.cn>
2019-06-12 09:34:57 +0800

    增加定时任务

--- article_api_celery/celery.py
+++ article_api_celery/celery.py
@@ -16,7 +16,7 @@ app.conf.beat_schedule = {
 
     # 更新文章 (10分钟一次)
     'update_article':{
-        'task':'tianyan_celery.tasks.update_article',
+        'task':'article_api_celery.tasks.update_article',
         # 'schedule': crontab("0", '*/1', '*', '*', '*'),  # 此处跟 linux 中 crontab 的格式一样
         'schedule': crontab(minute='*/10'),  # 直接写个10为1小时执行一次  */10 为 十分钟一次
         'args':[] # # 传入任务函数的参数,可以是一个列表或元组,如果函数没参数则为空列表或空元组
